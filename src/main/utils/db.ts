2c984793c9220282170aaa3955202fd565100dbc
Yojda <yanthekill@gmail.com>
2025-07-14 02:04:36 -0400

    [build] fix

--- src/main/utils/db.ts
+++ src/main/utils/db.ts
@@ -14,4 +14,4 @@ if (process.env.NODE_ENV === "production") {
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-export const db = drizzle(pool);
+export const prisma = drizzle(pool);
