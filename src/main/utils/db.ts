9c9a8e24feeac400e6071ff447b6896abca59c55
Yojda <yanthekill@gmail.com>
2025-07-14 12:41:30 -0400

    [fix] dependency

--- src/main/utils/db.ts
+++ src/main/utils/db.ts
@@ -1,22 +1,15 @@
-import { PrismaClient } from '@/generated/prisma';
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import dotenv from 'dotenv';
-import ws from 'ws';
+// next.config.js
+import {PrismaPlugin} from '@prisma/nextjs-monorepo-workaround-plugin'
 
-// Setup
-dotenv.config()
-neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin())
+    }
 
-// Init prisma client
-const adapter = new PrismaNeon({ connectionString })
+    return config
+  },
+}
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+module.exports = nextConfig
